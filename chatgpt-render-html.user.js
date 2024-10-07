824ec0123c2474c0b9377013ebc771bd925a1842
unknown <664485186@qq.com>
2024-10-07 13:46:55 +0800

    update

--- chatgpt-render-html.user.js
+++ chatgpt-render-html.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT 渲染HTML代码
 // @namespace    https://github.com/21888/chatgpt-render-html
-// @version      2024-10-07
+// @version      202410071345
 // @description  chatgpt渲染html代码块,像Claude一样实时预览html ( chatgpt renders HTML code blocks, previewing HTML in real-time like Claude )
 // @author       chabai
 // @match        https://chatgpt.com/c/*
